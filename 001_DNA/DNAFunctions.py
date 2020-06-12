f14b7aa2b904a442a2fd9c634b62c86f5b7b0d3b
McKenzie Hughes <60634173+hughesmca@users.noreply.github.com>
2020-06-11 20:22:15 -0700

    Update DNAFunctions.py

--- 001_DNA/DNAFunctions.py
+++ 001_DNA/DNAFunctions.py
@@ -2,7 +2,7 @@
 
 Nucleotides = [ "A", "C", "G", "T"]
 
-# Check the sequence to make sure it is a DNA String
+# Check nucleotide sequence for validity
 def validateSeq(dna_seq):
     tmpseq = dna_seq.upper()
     for nuc in tmpseq:
@@ -11,15 +11,17 @@ def validateSeq(dna_seq):
     return tmpseq
 
 
-# Creating a random DNA sequence for testing:
+# Create random DNA sequence for testing:
 import random
 randDNAStr = ''.join([random.choice(Nucleotides)
                 for nuc in range(50)])
 
 
-# Frequency
+# Frequency of Nucleotide
 def countNucFrequency(seq):
     tmpFreqDict = { "A": 0, "C": 0, "G": 0, "T": 0}
     for nuc in seq:
         tmpFreqDict[nuc] += 1
     return tmpFreqDict
+
+#Optimize Frequency using collections.py
